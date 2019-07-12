1d35c84e399e651a4c0c233177b08914676f667e
aravindmedamoni <aravnd.accurate@gmail.com>
2019-07-12 21:01:58 +0530

    initial commit..

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// The main function is the starting point of the all our flutter projects.
+// for indentation we can use {} in the place of =>
+void main() => runApp(
+      MaterialApp(
+        home: Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
